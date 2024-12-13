9a36ec59673af99920288a9db9f13a4469911999
brennm2 <brendon_marcello@yahoo.com>
2024-12-13 21:56:10 +0000

    add player light

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,4 +1,6 @@
 extends CharacterBody2D
+@onready var player_light: PointLight2D = $playerLight
+@onready var timer: Timer = $playerLight/Timer
 
 var SPEED = 100
 const JUMP_VELOCITY = -300.0
@@ -6,8 +8,21 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @export var coyote_time = 0.1
 var jump_available = true
 @onready var coyote_timer: Timer = $coyote_timer
+var texture_number = 0
+
+
+var LIGHT_1 = preload("res://assets/sprites/light1.png")
+var LIGHT_2 = preload("res://assets/sprites/light2.png")
+var LIGHT_3 = preload("res://assets/sprites/light3.png")
+var LIGHT_4 = preload("res://assets/sprites/light4.png")
+
+func _ready() -> void:
+	change_light_texture()
 
 func _physics_process(delta):
+	if (player_light.texture_scale > 0.5):
+		player_light.texture_scale -= 0.1 * delta
+	
 
 	if not is_on_floor():
 		if jump_available:
@@ -35,3 +50,25 @@ func _physics_process(delta):
 		
 func coyote_timeout():
 	jump_available = false
+
+func change_light_texture():
+	timer.start()
+	
+
+func _on_timer_timeout() -> void:
+	print("entrou")
+	if (texture_number == 0):
+		player_light.texture = LIGHT_1
+		texture_number += 1
+	elif (texture_number == 1):
+		player_light.texture = LIGHT_2
+		texture_number += 1
+	elif (texture_number == 2):
+		player_light.texture = LIGHT_3
+		texture_number +=1
+	elif (texture_number == 3):
+		player_light.texture = LIGHT_4
+		texture_number = 0
+	change_light_texture()
+		
+		
