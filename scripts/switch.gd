3a732d466b37eacbeab9adf060612084729d64fc
Jorge Costa <Kelho88@pm.me>
2024-12-14 15:33:22 +0000

    all merged

--- scripts/switch.gd
+++ scripts/switch.gd
@@ -6,7 +6,7 @@ extends Node2D
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.name == "player":
+	if body.is_in_group("player"):
 		on.visible = true
 		off.visible = false
 	else:
