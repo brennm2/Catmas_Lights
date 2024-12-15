29fa9867449014abb443453d15090f4bac9ff4f0
Jorge Costa <Kelho88@pm.me>
2024-12-15 18:38:42 +0000

    added timer

--- death_screen.gd
+++ death_screen.gd
@@ -1,5 +1,6 @@
 extends Control
 @onready var canvas_layer: CanvasLayer = $CanvasLayer
+@onready var timer: Timer = $Timer
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,14 +12,8 @@ func _process(delta: float) -> void:
 	pass
 
 
-
-#func _on_restart_button_pressed():
-	#var current_scene = get_tree().get_current_scene()
-	#print("Reloading scene: ", current_scene.filename)
-	#get_tree().change_scene(current_scene.filename)
-
 func _on_player_player_died() -> void:
-	canvas_layer.show()
+	timer.start()
 
 func _on_button_pressed() -> void:
 	if Globals.current_scene_path:
@@ -26,3 +21,7 @@ func _on_button_pressed() -> void:
 		Globals.lightScale = 5
 	else:
 		print("Error: Current scene path not set.")
+
+
+func _on_timer_timeout() -> void:
+	canvas_layer.show()
