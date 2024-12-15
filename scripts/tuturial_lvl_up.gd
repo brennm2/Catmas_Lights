b3880ef42775166fb5352d6b0c962176fa1f2fb2
Jorge Costa <Kelho88@pm.me>
2024-12-15 15:44:35 +0000

    hue

--- scripts/tuturial_lvl_up.gd
+++ scripts/tuturial_lvl_up.gd
@@ -12,5 +12,5 @@ func _ready() -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		if Globals.tutorial == 0:
+		if Globals.tutorial == 3:
 			get_tree().change_scene_to_packed(LVL_2)
