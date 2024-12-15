992db12d2e3d9f2cd72c08f5ac8a7e70b82b535f
brennm2 <brendon_marcello@yahoo.com>
2024-12-15 15:08:39 +0000

    More sounds and fixs

--- scripts/light_refill.gd
+++ scripts/light_refill.gd
@@ -3,6 +3,8 @@ extends Area2D
 @onready var animation_player = $AnimationPlayer
 @export var item_id : String
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var point_light_2d: PointLight2D = $PointLight2D
+@onready var timer: Timer = $Timer
 
 func _ready():
 	animated_sprite.play("default")
@@ -10,3 +12,13 @@ func _ready():
 func _on_body_entered(_body):
 	animation_player.play("pickUp")
 	Globals.lightScale = 5
+	timer.start()
+	#while(point_light_2d.energy >= 0):
+		#print(point_light_2d.energy)
+		#timer.start()
+
+
+func _on_timer_timeout() -> void:
+	if (point_light_2d.energy >= 0):
+		point_light_2d.energy -= 0.1
+	timer.start()
