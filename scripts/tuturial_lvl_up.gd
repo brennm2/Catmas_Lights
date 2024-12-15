7d344d94c03eafc934ff9ff090b765f48759034b
Jorge Costa <Kelho88@pm.me>
2024-12-15 15:55:43 +0000

    fixed conflicts

--- scripts/tuturial_lvl_up.gd
+++ scripts/tuturial_lvl_up.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
 
-
+#
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		if Globals.tutorial == 3:
