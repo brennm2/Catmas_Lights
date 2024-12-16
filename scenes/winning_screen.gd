4902d911779c40e88b0f113cb38d693855465349
Jorge Costa <Kelho88@pm.me>
2024-12-16 18:14:49 +0000

    Final Build

--- scenes/winning_screen.gd
+++ scenes/winning_screen.gd
@@ -8,6 +8,7 @@ func _ready() -> void:
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("HELLO")
 	if body.is_in_group("player"):
 		if Globals.boss == 9:
 			Engine.time_scale = 0.05
